77871dd62afe7f4f67ac35f96f0d80beb2a4d486
Mark Faust <markfaust123@gmail.com>
2024-01-17 16:24:56 -0500

    added styling to the PlaceForm to make TextInput and Title visible

--- src/components/Places/PlaceForm.tsx
+++ src/components/Places/PlaceForm.tsx
@@ -1,11 +1,43 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colors } from "../../lib/constants";
 
 const PlaceForm = () => {
-    return (
-        <View>
-            <Text>The Place Form</Text>
-        </View>
-    );
-}
+  const [enteredTitle, setEnteredTitle] = useState<string>("");
+
+  const handleChangeInputText = (text: string) => {
+    setEnteredTitle(text);
+  };
+
+  return (
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.input} onChangeText={handleChangeInputText} value={enteredTitle} />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
 
 export default PlaceForm;
